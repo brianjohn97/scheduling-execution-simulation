7c333576022f59eeac74c32feb8b91b65aa6a1fa
brianjohn97 <johnson.brian997@gmail.com>
2023-06-01 08:24:23 -0700

    update
    added load balancing function. fixed the scheduling algorithms

--- Lab5.cpp
+++ Lab5.cpp
@@ -3,9 +3,11 @@
 #include <vector>
 #include <pthread.h>
 #include <unistd.h>
+#include <algorithm>
 
 using namespace std;
 
+//struct for holding all the information about each process
 struct process {
     char name[32];
     int id;
@@ -19,12 +21,7 @@ struct process {
     int checkSum;
 };
 
-
-
 pthread_mutex_t myMutex = PTHREAD_MUTEX_INITIALIZER;
-
-//struct for holding all the information about each process
-
 
 //vector for holding all the structs of each process
 vector<process> processes;
@@ -33,9 +30,7 @@ vector<process> p1;
 vector<process> p2;
 vector<process> p3;
 
-vector<int> pos;
-
-int size = 0;
+int totalProc = 0;
 
 //read in the processes from the binary file name
 void readProcessesFromFile(string fileName){
@@ -49,12 +44,12 @@ void readProcessesFromFile(string fileName){
 
     //get the length of the file  found at https://cplusplus.com/reference/istream/istream/tellg/
     in.seekg(0, in.end);
-    size = in.tellg();
+    totalProc = in.tellg();
     in.seekg(0, in.beg);
 
-    size /= 63;
+    totalProc /= 63;
     //read the process information from the binary file
-    while (x < size){
+    while (x < totalProc){
 
         //create struct for holding the data
         process proc = {};
@@ -77,7 +72,7 @@ void readProcessesFromFile(string fileName){
         x++;
     }
     in.close();
-    cout << "\nThe number of processes available in the file: " << size << endl;
+    cout << "\nThe number of processes available in the file: " << totalProc << endl;
     cout << "Total number of memory allocated by the processes is: " << totalMem << endl;
     cout << "Overall total number of open files: " <<numOfFiles << endl << endl;
 
@@ -133,24 +128,93 @@ void print2(){
     }
 }
 void print3(){
-    int end = p0.size();
+    int end = p3.size();
     end--;
     for (int i = 0; i <= end; ++i) {
-        cout << "\nProcess["<<i<<"] Name: " << p0[i].name <<endl;
-        cout << "Process ID: " << p0[i].id << endl;
-        cout << "Activity Status: " << static_cast<int>(p0[i].status) << endl;
-        cout << "CPU Burst Time: " << p0[i].cpuTime << endl;
-        cout << "Base Register: " << p0[i].base << endl;
-        cout << "Limit Register: " << p0[i].limit << endl;
-        cout << "Process type: " << static_cast<int>(p0[i].type) << endl;
-        cout << "Number of files: " << p0[i].numFiles << endl;
-        cout << "Priority: " << static_cast<int>(p0[i].priority) << endl;
-        cout << "Check sum: " << p0[i].checkSum << endl << endl;
+        cout << "\nProcess["<<i<<"] Name: " << p3[i].name <<endl;
+        cout << "Process ID: " << p3[i].id << endl;
+        cout << "Activity Status: " << static_cast<int>(p3[i].status) << endl;
+        cout << "CPU Burst Time: " << p3[i].cpuTime << endl;
+        cout << "Base Register: " << p3[i].base << endl;
+        cout << "Limit Register: " << p3[i].limit << endl;
+        cout << "Process type: " << static_cast<int>(p3[i].type) << endl;
+        cout << "Number of files: " << p3[i].numFiles << endl;
+        cout << "Priority: " << static_cast<int>(p3[i].priority) << endl;
+        cout << "Check sum: " << p3[i].checkSum << endl << endl;
     }
 }
 
+//find the processor with the most processes
+//then distribute it the empty vector
+void loadBalancing(int empty){
+
+    //finds the processor with the most processes left
+    int proc1 = p0.size() - 1;
+    int proc2 = p1.size() - 1;
+    int proc3 = p2.size() - 1;
+    int proc4 = p3.size() - 1;
+    int largest = max({proc1, proc2, proc3, proc4});
+    int half = largest/2;
+    if(half <= 5){ return;}
+
+    //determines which processor has the most
+    cout << "\nTRANSFERRING\n\n";
+    if(largest == proc1){
+
+        //splits the proccessor amount in half then adds it to the one that is empty
+
+        for (int i = p0.size()-1; i >= half; --i) {
+            process temp = p0[i];
+            if(empty == 1){ p1.push_back(temp);}
+            if(empty == 2){ p2.push_back(temp);}
+            if(empty == 3){ p3.push_back(temp);}
+            p0.pop_back();
+        }
+    }else if(largest == proc2){
+
+        //splits the processor amount in half then adds it to the one that is empty
+
+        int half = largest/2;
+        if(half <= 5){ return;}
+        for (int i = p1.size()-1; i >= half; --i) {
+            process temp = p1[i];
+            if(empty == 0){ p0.push_back(temp);}
+            if(empty == 2){ p2.push_back(temp);}
+            if(empty == 3){ p3.push_back(temp);}
+            p1.pop_back();
+        }
+    }else if(largest == proc3){
+
+        //splits the processor amount in half then adds it to the one that is empty
+        int half = largest/2;
+        if(half <= 5){return;}
+        for (int i = p2.size()-1; i >= half; ++i) {
+            process temp = p2[i];
+            cout << "half: " << half << endl;
+            if(empty == 0){ p0.push_back(temp);}
+            if(empty == 1){ p1.push_back(temp);}
+            if(empty == 3){ p3.push_back(temp);}
+            p2.pop_back();
+        }
+
+    }else if (largest == proc4){
+
+        //splits the processor amount in half then adds it to the one that is empty
+        int half = largest/2;
+        if(half <= 5){return;}
+        for (int i = p3.size()-1; i >= half; ++i) {
+            process temp = p3[i];
+            if(empty == 0){ p0.push_back(temp);}
+            if(empty == 2){ p2.push_back(temp);}
+            if(empty == 1){ p1.push_back(temp);}
+            p3.pop_back();
+        }
+    }
+}
+
+//print all the processes
 void printProcesses(){
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < totalProc; ++i) {
         cout << "\nProcess["<<i<<"] Name: " << processes[i].name <<endl;
         cout << "Process ID: " << processes[i].id << endl;
         cout << "Activity Status: " << static_cast<int>(processes[i].status) << endl;
@@ -164,106 +228,148 @@ void printProcesses(){
     }
 }
 
+//priority scheduling thread function
 void * priority(void * arg){
+
+    //start variables
     int start = (int)(long)arg;
-    int end;
-    pthread_mutex_lock(&myMutex);
-    for (int i = 0, j = i+1; i < pos.size(); ++i, ++j) {
-        if(pos[i] == start){
-            end = pos[j];
-        }
-    }
-    //cout << "pos[i] : " << start << " pos[j] : " << end << endl;
-    print3();
+    int end =  p3.size() - 1;
 
-    pthread_mutex_unlock(&myMutex);
-
-    return NULL;
-}
-
-void * shortestJob(void * arg){
-    int start = (int)(long)arg;
-    int end;
-    pthread_mutex_lock(&myMutex);
-    cout << "\nShortest Job!: " << start << endl;
-//    for (int i = 0, j = i+1; i < pos.size(); ++i, ++j) {
-//        if(pos[i] == start){
-//            end = pos[j];
-//        }
-//    }
-//    //cout << "pos[i] : " << start << " pos[j] : " << end << endl;
-//    print2();
-    pthread_mutex_unlock(&myMutex);
-    return (void*)0;
-}
-
-void * roundRobin(void * arg){
-    int start = (int)(long)arg;
-    int end = p1.size() -1;
-    pthread_mutex_lock(&myMutex);
-    cout << "\nRound Robin!: " << start << endl;
-    print1();
-
-    int i=0;
-    while (true) {
-        if(p1.empty()){break;}
-        cout << p1[i].cpuTime << endl;
-        p1[i].cpuTime -= 2;
-        cout << p1[i].cpuTime << endl;
-        if(p1[i].cpuTime < 1){
-            cout << "RR: " <<  p1[i].name << " has finished!\nMoving on to the next Process!\n\n";
-            p1.erase(p1.begin() + i);
-            print1();
-            cout << "end: " << end << endl;
-            end--;
-            cout << "end: "<<end << endl;
-        }
-        usleep(20000);
-        cout << "i: " << i << " end: " << end <<  endl;
-        if(i == end){i = 0; continue;}
-        i++;
-    }
-    cout << "Round Robin has finished all of its processes!\n\n";
-    pthread_mutex_unlock(&myMutex);
-    return (void*)0;
-}
-
-void * firstComeFirstServe(void * arg){
-
-
-    //have the lock and unlock when it checks if its empty
-    //if empty start lock then check which vector is the biggest
-    //then divide that vectors size by half then add them to this vector
-
-    pthread_mutex_lock(&myMutex);
-    cout << "\nFirst Come First Serve!" << endl;
-    /*
-    print0();
-    cout << endl << endl;
-    int end = p0.size();
-    for (int i = 0; i <= end; ++i) {
-        if(p0.empty()){
-            while(1){
-                if(!p0.empty()){
-                    break;
-                }
+    //order them in terms of priority
+    for (int i = 0; i < end; ++i) {
+        for (int j = i+1; j < end+1; ++j) {
+            if(p3[i].priority > p3[j].priority){
+                swap(p3[i], p3[j]);
             }
         }
-        int time = p0[i].cpuTime;
-        while(time > 0){
+    }
+
+    while(true) {
+
+        //check if the vector is empty and then get more
+
+        if(p0.empty() && p1.empty() && p2.empty() && p3.empty()){break;}
+        if(p3.empty()){pthread_mutex_lock(&myMutex); loadBalancing(3); pthread_mutex_unlock(&myMutex);}
+        if(p3.size() < 1){ while(1){if(p0.empty() && p1.empty() && p2.empty() && p3.empty()){return (void*)0;}}}
+
+        //subtract cpu time  until below 1 then remove it and announce it
+        int time = p3[0].cpuTime;
+        while(time > 1){
             time -= 2;
-            //cout << time << endl;
             usleep(20000);
         }
 
+        cout << "Priority: " <<  p3[0].name << " has finished!\nMoving on to the next Process!\n\n";
+        p3.erase(p3.begin());
+    }
+    return (void*)0;
+}
+
+//shortest job scheduling thread function
+void * shortestJob(void * arg){
+
+    //start variables
+    int start = (int)(long)arg;
+    int end = p2.size() - 1;
+
+    //order the processes with the least amount of cpu time to the most
+    for (int i = 0; i < end; ++i) {
+        for (int j = i+1; j < end+1; ++j) {
+            if(p2[i].cpuTime > p2[j].cpuTime){
+                swap(p2[i], p2[j]);
+            }
+        }
+    }
+
+
+    while(true) {
+
+        //check if the vector if empty and call load balancing to get more
+        if(p0.empty() && p1.empty() && p2.empty() && p3.empty()){break;}
+        if(p2.empty()){pthread_mutex_lock(&myMutex); loadBalancing(2); pthread_mutex_unlock(&myMutex);}
+        if(p2.size() < 1){ while(1){if(p0.empty() && p1.empty() && p2.empty() && p3.empty()){return (void*)0;}}}
+
+
+        //subtract time one at a time until its below 1 then remove it and announce it
+        int time = p2[0].cpuTime;
+        while(time > 1){
+            time -= 2;
+            usleep(20000);
+        }
+        cout << "SJF: " <<  p2[0].name << " has finished!\nMoving on to the next Process!\n\n";
+        p2.erase(p2.begin());
+    }
+    return (void*)0;
+}
+
+//round-robin scheduling thread function
+void * roundRobin(void * arg){
+
+    //start variables
+    int start = (int)(long)arg;
+    int end = p1.size() -1, i = 0;
+
+    while (true) {
+
+        //check if the vector is empty and call load balancing to get more processes
+
+        if(p0.empty() && p1.empty() && p2.empty() && p3.empty()){break;}
+        if(p1.empty()){pthread_mutex_lock(&myMutex); loadBalancing(1); pthread_mutex_unlock(&myMutex);}
+        if(p1.size() < 1){ while(1){if(p0.empty() && p1.empty() && p2.empty() && p3.empty()){return (void*)0;}}}
+
+        //if all the processes have been completed exit.
+
+        //cout << "p1 size: " << p1.size()   << " p0 size: " << p0.size() << endl;
+        //subtract the cpu time from each  and if one gets below 1 then remove it and announce it
+        p1[i].cpuTime -= 2;
+        if(p1[i].cpuTime < 1){
+            cout << "RR: " <<  p1[i].name << " has finished!\nMoving on to the next Process!\n\n";
+            p1.erase(p1.begin() + i);
+            end--;
+            if(end == 0){i = 0;}
+        }
+
+        usleep(20000);
+        if(i == end || i > end){i = 0; continue;}
+        i++;
+    }
+    return (void*)0;
+}
+
+//FCFS thread algorithm
+void * firstComeFirstServe(void * arg){
+    //starting variables
+    int end = p0.size()-1;
+    int i = 0;
+
+    //while loop to simulate running the processes
+    while (true) {
+
+        //checks if the queue is empty and if so it will then load balance
+        //to get more process to run
+
+        if(p0.empty() && p1.empty() && p2.empty() && p3.empty()){break;}
+        if(p0.empty()){pthread_mutex_lock(&myMutex); loadBalancing(0); pthread_mutex_unlock(&myMutex);}
+        if(p0.size() < 1){ while(1){if(p0.empty() && p1.empty() && p2.empty() && p3.empty()){return (void*)0;}}}
+
+        //if there are no more processes then break
+
+        //cout << "p0 size: " << p0.size() << endl;
+        //get the cpu time and inside a loop bring it down to 0
+        int time = p0[i].cpuTime;
+        while(time > 0){
+            time -= 2;
+            usleep(20000);
+        }
+
+        //print what process has finished and erase it from the vector
         cout << "FCFS: " <<  p0[0].name << " has finished!\nMoving on to the next Process!\n\n";
         p0.erase(p0.begin());
-        print0();
+
+        if(end == 0){i = 0;}
+        if(i == end){i = 0; continue;}
+        i++;
     }
-    cout << "First come first Serve has finished all of its processes!\n\n";
-    if(p0.empty()){cout << "true\n";}
-    //print0();*/
-    pthread_mutex_unlock(&myMutex);
     return (void*)0;
 }
 
@@ -332,19 +438,25 @@ void printWhatsMissing(int argc, char **argv){
 //then starts a thread
 void getProcessors(int argc, char **argv){
 
+    //start varibles
     int sched;
     float perc;
     float max = 1.0;
     float total = 0;
-    int x, temp, start;
+    int x, temp, start, k = 0;
     pthread_t myThreads[10];
+
+    //finds what is missing from the command line arugments
     printWhatsMissing(argc, argv);
-
-
-    int k=0;
+    printProcesses();
+    /*
     for (int i = 2, j = i + 1; i < argc; i += 2, j += 2, k++) {
+
+        //get the start position
         if(i == 2){start = 0;}
         else{start = temp;}
+
+        //try and get the int and double from the user
         try {
             sched = stoi(argv[i]);
             perc = stof(argv[j]);
@@ -354,49 +466,78 @@ void getProcessors(int argc, char **argv){
             exit(0);
         }
 
-        x = perc * size;
+        //calculate the amount of processes from the percentage and total amount of processes
+        x = perc * totalProc;
         x--;
         if(i != 2){x += temp;}
-        //cout <<  "x: " << x << " temp: " << temp << endl;
 
+        //depending on the sceduling algorithm chosen by user
+        //it will then add the correct processes to the correct vector then create the thread for that algorithm
         if(sched == 0){
-            //cout << "start: " << start << " x: " << x << endl;
+
             for (int l = start; l <= x; ++l) {
                 p0.push_back(processes[l]);
             }
             pthread_create(&myThreads[k], 0, firstComeFirstServe, (void *)(long)start);
+            if(perc == 1){
+                while (1){
+                    if(p0.empty() && p1.empty() && p2.empty() && p3.empty()){break;}
+                }
+            }
         }else if (sched == 1){
-            //cout << "start: " << start << " x: " << x << endl;
+
             for (int l = start; l <= x; ++l) {
                 p1.push_back(processes[l]);
             }
+
             pthread_create(&myThreads[k], 0, roundRobin, (void *)(long)start);
+            if(perc == 1){
+                while (1){
+                    if(p0.empty() && p1.empty() && p2.empty() && p3.empty()){break;}
+                }
+            }
         }else if (sched == 2){
-            //cout << "start: " << start << " x: " << x << endl;
+
             for (int l = start; l <= x; ++l) {
                 p2.push_back(processes[l]);
             }
+
             pthread_create(&myThreads[k], 0, shortestJob, (void *)(long)start);
+            if(perc == 1){
+                while (1){
+                    if(p0.empty() && p1.empty() && p2.empty() && p3.empty()){break;}
+                }
+            }
         }else if (sched == 3){
-            //cout << "start: " << start << " x: " << x << endl;
+
             for (int l = start; l <= x; ++l) {
                 p3.push_back(processes[l]);
             }
-            pthread_create(&myThreads[k], 0, priority, (void *)(long)start);
-        }
 
-        if(i == 2){pos.push_back(0); pos.push_back(x);temp = ++x; continue;}
-        pos.push_back(temp); pos.push_back(x);
+            pthread_create(&myThreads[k], 0, priority, (void *)(long)start);
+            if(perc == 1){
+                while (1){
+                    if(p0.empty() && p1.empty() && p2.empty() && p3.empty()){break;}
+                }
+            }
+        }
         temp = ++x;
     }
 
+    //join all the threads at the end
     for (int i = 1; i < k; i++){
         pthread_join(myThreads[i], NULL);
-    }
+    }*/
 }
 int main(int argc, char *argv[]){
     getProcessors(argc, argv);
-
+//    cout << "\n round Robin: \n\n";
+//    print1();
+//    cout << "SJF: \n\n";
+//    print2();
+//    cout << "PRiority: \n\n";
+//    print3();
+//    cout << "\nAll the processes have been finished!\n\n";
 
     return 0;
 }
